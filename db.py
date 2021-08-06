2ef4df4845bcdf96e5adaf097ca9e39b409ed7f7
Dmytro Dotsenko <dimaih577@gmail.com>
2021-08-06 13:58:02 +0300

    date & path formats

--- db.py
+++ db.py
@@ -1,22 +1,25 @@
+import os
+import random
+
 from json import JSONEncoder, JSONDecoder
 import datetime
 
 
-class Todo:
-    _id = 0
+data_path = "data/"
 
-    def __init__(self, task, done=False, date=datetime.datetime.now(), task_id=0):
+
+class Todo:
+    def __init__(self, task, done=False, date=None, task_id=0):
         self.task = task
         self.done = done
-        self.date = date
-        self.id = task_id if task_id else Todo._id
-        Todo._id = task_id + 1
+        self.date = date if date else datetime.datetime.now()
+        self.id = task_id if task_id else int(random.random() * 5000)  #Todo._id
 
 
 class JsonEncoder(JSONEncoder):
     def default(self, o):
         if isinstance(o, datetime.datetime):
-            return str(o)
+            return str(o.isoformat())
         else:
             return o.__dict__
 
@@ -25,21 +28,24 @@ def from_json(json_object):
     try:
         return Todo(json_object['task'],
                     json_object['done'],
-                    json_object['date'],
+                    datetime.datetime.fromisoformat(json_object['date']),
                     json_object['id'])
     except KeyError:
         return
 
 
 def read_json():
-    with open('data/todo_list.json', 'r') as read_file:
-        todo_list = []
-        data = read_file.read()
+    try:
+        with open(os.path.join(data_path, "todo_list.json"), 'r') as read_file:
+            todo_list = []
+            data = read_file.read()
         if data:
             todo_list = JSONDecoder(object_hook=from_json).decode(data)
         return todo_list
+    except FileNotFoundError:
+        return []
 
 
 def write_json(todo_list):
-    with open('data/todo_list.json', 'w') as out_file:
+    with open(os.path.join(data_path, "todo_list.json"), 'w') as out_file:
         out_file.write(JsonEncoder().encode(todo_list))
